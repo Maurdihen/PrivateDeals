083770cc2527eb1d9f4a958f8ccf109f1f172cc4
egorarhipov <arhipov_0406@mail.ru>
2023-01-24 23:24:58 +0300

    ExcelSaving v1.0.1

--- ExcelSaving/excel_class.py
+++ ExcelSaving/excel_class.py
@@ -6,6 +6,22 @@ class ExcelStudend:
     def __init__(self, fullname: str, age: int,
                  school: str, phone_number: str, photo: str, email: str, family: str, mom_number: str,
                  dad_number: str, mom_work: str, dad_work: str, floor: str, group: str):
+        '''
+        Иницилизация класса
+        :param fullname: str
+        :param age: int
+        :param school: str
+        :param phone_number: str
+        :param photo: str
+        :param email: str
+        :param family: str
+        :param mom_number: str
+        :param dad_number: str
+        :param mom_work: str
+        :param dad_work: str
+        :param floor: str
+        :param group: str
+        '''
         self.fullname = fullname
         self.age = age
         self.school = school
@@ -21,13 +37,43 @@ class ExcelStudend:
         self.group = group
 
 
+    def __repr__(self):
+        '''
+        Вызываеться при выводе экземпляра
+        :return: str
+        '''
+        return '''Принимает в себя 13 элементов: 
+        ФИО
+        Возраст
+        Школу
+        Номер телефона
+        Путь на фотку
+        Почту
+        Состав семьи
+        Мамин номер телефона
+        Папин номер телефона
+        Работа мамы
+        Работа папы
+        Пол
+        Группа
+        '''
+
+
     def run(self):
+        '''
+        Главная функция посредством котоорой запускаеться весь класс
+        :return:
+        '''
         if os.stat("data/students.csv").st_size == 0:
             ExcelStudend.add_columns_name(self)
         ExcelStudend.add_main_information(self)
 
 
     def add_main_information(self):
+        '''
+        Записывает/дозаписывает в данные в таблицу
+        :return:
+        '''
         with open('data/students.csv', 'a', newline='') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow([self.fullname, self.age, self.school, self.phone_number, self.photo, self.email,
@@ -36,6 +82,10 @@ class ExcelStudend:
 
 
     def add_columns_name(self):
+        '''
+        Добавление названий таблицы
+        :return:
+        '''
         with open('data/students.csv', 'a', newline='') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow(["Full name", "Age", "School", "Mobile phone", "Path to photo", "Email",
