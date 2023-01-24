3f928b6e257f8885deb6711d39d144b9445bc71a
egorarhipov <arhipov_0406@mail.ru>
2023-01-24 23:10:33 +0300

    ExcelSaving v1.0.1

--- ExcelSaving/excel_class.py
+++ ExcelSaving/excel_class.py
@@ -3,7 +3,7 @@ import os
 class ExcelStudend:
     def __init__(self, fullname: str, age: int,
                  school: str, phone_number: str, photo: str, email: str, family: str, mom_number: str,
-                 dad_number: str, mom_work: str, dad_work: str):
+                 dad_number: str, mom_work: str, dad_work: str, floor: str, group: str):
         self.fullname = fullname
         self.age = age
         self.school = school
@@ -15,6 +15,8 @@ class ExcelStudend:
         self.dad_number = dad_number
         self.mom_work = mom_work
         self.dad_work = dad_work
+        self.floor = floor
+        self.group = group
     def run(self):
         if os.stat("data/students.csv").st_size == 0:
             ExcelStudend.add_columns_name(self)
@@ -23,12 +25,13 @@ class ExcelStudend:
         with open('data/students.csv', 'a', newline='') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow([self.fullname, self.age, self.school, self.phone_number, self.photo, self.email,
-                             self.family, self.mom_number, self.dad_number, self.mom_work, self.dad_work])
+                             self.family, self.mom_number, self.dad_number, self.mom_work, self.dad_work, self.floor,
+                             self.group])
     def add_columns_name(self):
         with open('data/students.csv', 'a', newline='') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow(["Full name", "Age", "School", "Mobile phone", "Path to photo", "Email",
-                             "Family","Mom's number", "Dad's number", "Mom's work", "Dad's work"])
+                             "Family","Mom's number", "Dad's number", "Mom's work", "Dad's work", "Floor", "Group"])
 
 
 excel = ExcelStudend('sfs', 3, 'sdf', 'sdf', 'sdf', 'sdf', 'sdf', 'sdf', 'sdf', 'sdf', 'sdf')
